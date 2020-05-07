571a52e2b01d23554ce1071fbb0103795f88a232
Yoom Davidson <ddavidson202020@gmail.com>
2020-05-06 20:33:39 -0400

    Get the sort first name to work

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,29 +6,65 @@ class Main extends React.Component {
     state = {
         employees: [],
         search: "",
-        filteredEmployees: []
+        filteredEmployees: [],
+        order: "descend"
     }
 
     componentDidMount() {
         API.getEmployees()
             .then(res => {
                 this.setState({ employees: res.data.results, filteredEmployees: res.data.results })
-            
+
             })
             .catch(err => console.log(err));
     }
 
     handleInput = (event) => {
-        this.setState({search: event.target.value});
+        this.setState({ search: event.target.value });
         //const searchValue = event.target.value;
         //Use the filter method to filter employees according to what user types in 
         const filtered = this.state.filteredEmployees.filter((employee) => {
-            
-          return employee.name.first.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            return employee.name.first === this.state.search
+            //return employee.name.first.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
             //return employee.name.first === searchValue
         })
         // Set the state of filterEmployees 
-        this.setState({filteredEmployees: filtered})
+        this.setState({ filteredEmployees: filtered })
+    }
+
+    handleSortFirst = () => {
+        // a function that sorts the array according to their first names
+        function compare1(a,b) {
+           const firstA = a.name.first.toLowerCase();
+           const firstB = b.name.first.toLowerCase();
+           let comparison = 0;
+           if (firstA > firstB) {
+               comparison =1;
+           } else if (firstA < firstB) {
+               comparison = -1;
+           }
+           return comparison;
+        }
+        // a function that sorts the array according to their last names
+        // function compare2(a,b) {
+        //     const firstA = a.name.first.toLowerCase();
+        //     const firstB = b.name.first.toLowerCase();
+        //     let comparison = 0;
+        //     if (firstA > firstB) {
+        //         comparison =1;
+        //     } else if (firstA < firstB) {
+        //         comparison = -1;
+        //     }
+        //     return comparison * -1;
+        // }
+
+        const sortA = this.state.employees.sort(compare1);
+        //const sortZ = this.state.employees.sort(compare2);
+        this.setState({employees: sortA});
+        //this.setState({employees: sortZ});
+    }
+
+    handleSortLast = () => {
 
     }
 
@@ -52,8 +88,8 @@ class Main extends React.Component {
         return (
             <div>
                 <Header
-                handleInput={this.handleInput}
-                value={this.state.search}
+                    handleInput={this.handleInput}
+                    value={this.state.search}
                 />
                 <div className={"container-fluid"}>
                     <div className={"table-responsive"}>
@@ -61,16 +97,16 @@ class Main extends React.Component {
                             <thead>
                                 <tr>
                                     <th>Image</th>
-                                    <th>First Name<i className={"fa fa-fw fa-sort"}></i></th>
-                                    <th>Last Name<i className={"fa fa-fw fa-sort"}></i></th>
+                                    <th>First Name<i onClick={this.handleSortFirst} className={"fa fa-fw fa-sort"}></i></th>
+                                    <th>Last Name<i onClick={this.handleSortLast} className={"fa fa-fw fa-sort"}></i></th>
                                     <th>Email</th>
                                     <th>Age<i className={"fa fa-fw fa-sort"}></i></th>
                                     <th>City</th>
-                                    <th>State<i className={"fa fa-fw fa-sort"}></i></th>
+                                    <th>State</th>
                                 </tr>
                             </thead>
                             {/* if the state of search is true meaning there's input in the search then display the filteredEmployees, if not display employeeInfo which was declared above the return */}
-                            {this.state.search ? this.state.search : employeeInfo}
+                            {this.state.search ? this.handleInput : employeeInfo}
                         </table>
                     </div>
                 </div>
